f12c8ba625ff5f2642ccd4c44b1b99d351a4bf69
Thiago Baia <thiagobaia@outlook.com>
2024-07-09 14:38:37 -0300

    backgrounds
    backgrounds

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// // ATIVAR MENU
+// ATIVAR MENU
 
 // document.addEventListener("DOMContentLoaded", () => {
 //   const menuItems = document.querySelectorAll(".menu-ativo a");
@@ -20,7 +20,45 @@
 //       item.classList.add("active");
 //     });
 //   });
-// });
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuItems = document.querySelectorAll(".menu-ativo a");
+  const btnMobile = document.getElementById("btn-mobile");
+  const menuSpande = document.getElementById("menu-spande");
+
+  // Verifica se a página atual é a raiz "/"
+  if (window.location.pathname === '/') {
+    menuItems.forEach((item) => {
+      if (item.innerText.toLowerCase().includes("home")) {
+        item.classList.add("active");
+      }
+    });
+  }
+
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      // Remove a classe 'active' de todos os links
+      menuItems.forEach((link) => link.classList.remove("active"));
+      // Adiciona a classe 'active' ao link clicado
+      item.classList.add("active");
+
+      // Verifica se algum elemento tem a classe 'active'
+      if (document.querySelector(".menu-ativo a.active")) {
+        // Remove a classe 'flex' e adiciona a classe 'hidden' ao menu-spande
+        menuSpande.classList.remove("flex");
+        menuSpande.classList.add("hidden");
+
+        // Remove a classe 'is-active' do btn-mobile
+        btnMobile.classList.remove("is-active");
+      }
+    });
+  });
+});
+
+
 
 // MENU HAMBURGER
 
