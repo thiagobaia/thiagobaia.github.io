3b64667431f514a2d504612bf2ad5a5636de156e
Thiago Baia <thiagobaia@outlook.com>
2024-07-19 01:57:54 -0300

    19-07-24
    feat: 19-07-24

--- form.js
+++ form.js
@@ -8,15 +8,21 @@ function sendMail() {
   const serviceID = "service_llaiv6c";
   const templateID = "template_4i03s19";
 
-    emailjs.send(serviceID, templateID, params)
-    .then(res=>{
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Your message sent successfully!!")
+  emailjs.send(serviceID, templateID, params)
+    .then(res => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("message").value = "";
+      console.log(res);
+      
+      // Mostra o parágrafo de sucesso
+      var successMessage = document.getElementById("successMessage");
+      successMessage.style.display = "block";
 
+      // Esconde o parágrafo após 6 segundos
+      setTimeout(function() {
+        successMessage.style.display = "none";
+      }, 6000); // 6000 milissegundos = 6 segundos
     })
-    .catch(err=>console.log(err));
-
+    .catch(err => console.log(err));
 }
