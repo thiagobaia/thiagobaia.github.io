fca72128820c5e2e543d68a30604a189426c7bcd
Thiago Baia <thiagobaia@outlook.com>
2024-07-28 20:19:06 -0300

    ss
    ss

--- form.js
+++ form.js
@@ -29,18 +29,3 @@ function sendMail() {
 
 
 
-
-    let form = document.querySelector('#register');
-    form.addEventListener('submit',  e => {
-        e.preventDefault();
-        let name = document.querySelector('#name');
-        let email = document.querySelector('#email');
-        if(!name.value || !email.value) {
-            alert('Nome e Email são Obrigatorios');
-            } else{
-            grecaptcha.execute();
-        }       
-        });
-    function sendForm(){
-        alert("reCAPTCHA funcionou");
-    }
